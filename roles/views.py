17d6e24a6d388b50e1eabacbc62e4c508e9db195
Jesús Coello <jescoevas@alum.us.es>
2020-03-06 20:15:54 +0100

    Login y signup mejorados

--- roles/views.py
+++ roles/views.py
@@ -1,27 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import Owner
+from .models import Owner, Customer
 
 # Create your views here.
 
 def signup(request):
+    template = 'signup.html'
     if request.method == 'GET':
-        template='signup.html'
         return render(request, template)
     
     name = request.POST.get('name')
     surname = request.POST.get('surname')
     email = request.POST.get('email')
     phone = request.POST.get('phone')
+    role = request.POST.get('role')
     password = request.POST.get('password')
 
-    try:
-        owner = Owner(name=name, surname=surname, email=email, phone=phone, password=password)
-        owner.save()
-        return redirect('/login')
-    except:
-        messages.error(request,'Sign up failed')
-        return render(request,template)
+    if role == 'owner':
+        print('Dentro de owner')
+        try:
+            owner = Owner(name=name, surname=surname, email=email, phone=phone, password=password)
+            owner.save()
+            return redirect('/login')
+        except:
+            messages.error(request,'Sign up failed')
+            return render(request,template)
+    else:
+        print('Dentro de customer')
+        try:
+            customer = Customer(name=name, surname=surname, email=email, phone=phone, password=password)
+            customer.save()
+            return redirect('/login')
+        except:
+            messages.error(request,'Sign up failed')
+            return render(request,template)
+
+    
     
 
 def login(request):
@@ -31,10 +45,23 @@ def login(request):
     
     email = request.POST.get('email')
     password = request.POST.get('password')
-    
+    context = {'email_exists':email}
+
     try:
         owner = Owner.objects.get(email=email)
-        return render(request,template)
+        if owner.password == password:
+            return redirect('/index')
+        else:
+            context['password_is_not_correct'] = 'Password is not correct'
+            return render(request,template,context)
     except:
-        messages.error(request,'Owner does not exist')
-        return render(request,template)
+        try:
+            customer = Customer.objects.get(email=email)
+            if customer.password == password:
+                return redirect('/index')
+            else:
+                context['password_is_not_correct'] = 'Password is not correct'
+                return render(request,template,context)
+        except:
+            context['user_does_not_exist'] = 'User does not exist'
+            return render(request,template,context)
